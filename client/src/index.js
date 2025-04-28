280ed5b1402979219c01cea31e06ab5376b296da
Preston Jackson <156366687+Charger1423jr@users.noreply.github.com>
2025-04-28 12:57:42 -0400

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import Login from './Login';
 import './styles.css';
+
+function LoginFirst() {
+  const [user, setUser] = useState(null);
+
+  return (
+    <BrowserRouter>
+      {user ? <App /> : <Login setUser={setUser} />}
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <LoginFirst />
   </React.StrictMode>
 );
